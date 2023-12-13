b56d2bc88673323a929e6119c17168f123f0344e
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-12-13 17:13:11 +0000

    Linting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "home#index"
+  root to: 'home#index'
   get 'home/index'
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
