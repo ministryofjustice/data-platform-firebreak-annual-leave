168ebe40f6dd13ecef30e7ee003ec051277c5578
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-12-15 15:26:39 +0000

    Trying to get js working

--- scripts/devcontainer/post-create.sh
+++ scripts/devcontainer/post-create.sh
@@ -22,10 +22,3 @@ gem install rubocop
 gem update
 
 bundle install
-
-npm install govuk-frontend \
-    && cp node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.css app/assets/stylesheets \
-    && cp -r node_modules/govuk-frontend/dist/govuk/assets/images app/assets/images \
-    && cp -r node_modules/govuk-frontend/dist/govuk/assets/fonts app/assets/fonts \
-    && cp node_modules/govuk-frontend/dist/govuk/assets/manifest.json app/assets \
-    && cp node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js app/javascript
