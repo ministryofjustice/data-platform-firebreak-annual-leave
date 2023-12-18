e8bb6b047262596e0c30e4d084315bd2a8920c39
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-12-18 16:00:44 +0000

    Linting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // import '@hotwired/turbo-rails'
 // import 'controllers'
-import 'govuk-frontend';
+import 'govuk-frontend'
 
 // import { initAll } from '../../app/javascript/govuk-frontend.min.js';
-import { initAll } from '../../node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js';
-initAll();
+import { initAll } from '../../node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js'
+initAll()
 
 // This is right ^ I'm sure, you get errors in the console if you include it in the application.html.erb
