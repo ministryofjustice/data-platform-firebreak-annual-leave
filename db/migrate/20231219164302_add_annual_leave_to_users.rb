101d2a77888bebadf26eaf79e3a9067204869b62
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-12-19 16:44:10 +0000

    DB: Users, annual_leave default: 25

--- db/migrate/20231219164302_add_annual_leave_to_users.rb
+++ db/migrate/20231219164302_add_annual_leave_to_users.rb
@@ -1,5 +1,5 @@
-class AddAnnualLeaveToUsers < ActiveRecord::Migration[7.1]
+class AddAnnualLeaveToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :annual_leave, :integer
+    add_column :users, :annual_leave, :integer, default: 25
   end
 end
