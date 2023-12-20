89e92e1b0e89ce51a722eb8e072354202fc6636b
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-12-20 12:14:21 +0000

    :test_tube: Add tests for models

--- test/models/leave_record_test.rb
+++ test/models/leave_record_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class LeaveRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save LeaveRecord without required attributes" do
+    leave_record = LeaveRecord.new
+    assert_not leave_record.save, "Saved the leave_record without required attributes"
+  end
+
+  test "should belong to user" do
+    leave_record = leave_records(:johns_leave)
+    assert_equal users(:john), leave_record.user
+  end
+
+  test "end_date should be after start_date" do
+    leave_record = leave_records(:johns_leave)
+    assert leave_record.end_date > leave_record.start_date, "end_date is not after start_date"
+  end
+
+  test "should not allow overlapping leave records for the same user" do
+    overlapping_leave = LeaveRecord.new(user: users(:john), start_date: '2023-01-05', end_date: '2023-01-15')
+    assert_not overlapping_leave.save, "Saved overlapping leave records for the same user"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save User without email" do
+    user = User.new
+    assert_not user.save, "Saved the user without an email"
+  end
+  
+  test "email should be unique" do
+    user = User.new(email: "john@example.com")
+    assert_not user.save, "Saved a user with a duplicate email"
+  end
 end
