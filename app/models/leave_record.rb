7fab5b313ad16adf03a5835333fb890121073502
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-12-20 14:42:56 +0000

    :safety_vest: Update validations for leave_record Model

--- app/models/leave_record.rb
+++ app/models/leave_record.rb
@@ -1,6 +1,7 @@
 class LeaveRecord < ApplicationRecord
   belongs_to :user
 
+  validate :no_duplicate_booking
   validate :end_date_after_start_date
   validate :no_overlap, if: -> { start_date && end_date }
 
@@ -8,14 +9,19 @@ class LeaveRecord < ApplicationRecord
 
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
-
     errors.add(:end_date, "must be after the start date") if end_date < start_date
   end
 
   def no_overlap
     overlapping_leaves = LeaveRecord.where(user_id: user_id)
-                                     .where("start_date < ? AND end_date > ?", end_date, start_date)
+                                    .where.not(id: id) # Exclude the current record if it's already saved
+                                    .where("(start_date <= ? AND end_date >= ?) OR (start_date >= ? AND start_date <= ?) OR (end_date >= ? AND end_date <= ?)", start_date, start_date, start_date, end_date, start_date, end_date)
     
     errors.add(:base, "overlaps with existing leave") if overlapping_leaves.exists?
+  end
+
+  def no_duplicate_booking
+    existing_record = LeaveRecord.where(user_id: user_id, start_date: start_date, end_date: end_date).exists?
+    errors.add(:base, "Leave for these dates already booked") if existing_record
   end
 end
