45828a272f3268628bb25f3e5c530dfc57acf4d8
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-12-20 11:15:26 +0000

    Typo: ActiveRecord::Migration[6.0] - [7.1]

--- db/migrate/20231219164302_add_annual_leave_to_users.rb
+++ db/migrate/20231219164302_add_annual_leave_to_users.rb
@@ -1,4 +1,4 @@
-class AddAnnualLeaveToUsers < ActiveRecord::Migration[6.0]
+class AddAnnualLeaveToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :annual_leave, :integer, default: 25
   end
