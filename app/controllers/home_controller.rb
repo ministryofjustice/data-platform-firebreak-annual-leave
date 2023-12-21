c84fbc5ab7e7a70d7739d73ffdbf44d0c97e5642
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-12-21 15:46:42 +0000

    Calculated remaining leave

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,6 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @used_leave = current_user.used_leave
+    @remaining_leave = current_user.annual_leave - @used_leave
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[azure_activedirectory_v2]
 
   has_many :leave_records
+
+  def used_leave
+    @leave_records = self.leave_records
+    used_leave = 0
+
+    # Count weekdays between
+    @leave_records.each do |leave_record|
+      (leave_record.start_date...leave_record.end_date).each{|d| used_leave+=1 if (1..5).include?(d.wday)}
+    end
+
+    used_leave
+  end
 end
