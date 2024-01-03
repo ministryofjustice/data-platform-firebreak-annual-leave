c3e58e259ab74e613a7b1e996d1f65c0870cca1c
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2024-01-03 11:05:18 +0000

    Added user teams model tests

--- test/models/user_team_test.rb
+++ test/models/user_team_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    UserTeam.create(user_id: users(:john).id, team_id: teams(:DPL).id)
+    UserTeam.create(user_id: users(:jane).id, team_id: teams(:Tools).id)
+  end
+
+  test "User must exist" do
+    userteam = UserTeam.new(user_id: 999, team_id: teams(:DPL).id)
+    assert_not userteam.save, "Saved a non-existent user"
+  end
+
+  test "Team must exist" do
+    userteam = UserTeam.new(user_id: users(:john).id, team_id: 999)
+    assert_not userteam.save, "Saved a non-existent team"
+  end
+
+  test "User/Team relationship must be unique" do
+    userteam = UserTeam.new(user_id: users(:john).id, team_id: teams(:DPL).id)
+    assert_not userteam.save, "Saved an existing user team relationship"
+  end
+
+  test "Users can be added to multiple teams" do
+    userteam = UserTeam.new(user_id: users(:john).id, team_id: teams(:Tools).id)
+    assert userteam.save, "User not able to be added to multiple teams"
+  end
 end
