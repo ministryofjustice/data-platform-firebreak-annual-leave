0f90b28bca885fe8587fc289a2cc318ab7023fd0
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2024-01-03 10:38:14 +0000

    Added tests for team Model

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Team.create(name: "HR")
+  end
+
+  test "Team names must not be blank." do
+    team = Team.new
+    assert_not team.save, "Saved a team with no name"
+  end
+
+  test "Team names must not be duplicated." do
+    team = Team.create(name: "HR")
+    assert_not team.save, "Saved a team with a duplicate name"
+  end
 end
