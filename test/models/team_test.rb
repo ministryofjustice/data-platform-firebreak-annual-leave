8598046b56d0b09427d49658c472349d75cd1810
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2024-01-03 11:05:04 +0000

    Removed unneccessary test setup

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,17 +1,13 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  setup do
-    Team.create(name: "HR")
-  end
-
   test "Team names must not be blank." do
     team = Team.new
     assert_not team.save, "Saved a team with no name"
   end
 
   test "Team names must not be duplicated." do
-    team = Team.create(name: "HR")
+    team = Team.create(name: "DPL")
     assert_not team.save, "Saved a team with a duplicate name"
   end
 end
